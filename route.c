ad39e793c965d6dfc88bf0d1b7f10e4e4dcbfb92
Mengsk8086 <mengsk8086@gmail.com>
2013-05-11 01:35:06 +0200

    chg

--- route.c
+++ route.c
@@ -32,98 +32,7 @@ void route(void)
     u16 dcenter;
     while(1)
     {
-        
-        // ****************************************************************************************************************************
-        // ****************************************************************************************************************************
-        /*
-        //test du bloc infrarouge
-        
-        //dcenter = Distance_Get(Center);     // le robot s'arrête lorqu'il détecte un obstacle devant
-        //if (dcenter < 10)
-        //{
-        //  Motor_Left_Set(0);
-        //  Motor_Right_Set(0);
-        //  while(1)
-        //  {}
-        //}
-        
-        if (Direction >= 0)                  // le robot réduit son moteur droit si la balise est vers la droite
-        {
-        Motor_Right_Set(diffright-20*Direction);
-        Motor_Left_Set(diffleft);
-    }
-        if (Direction < 0)                  // le robot réduit son moteur gauche si la balise est vers la gauche
-        {
-        Motor_Right_Set(diffright);
-        Motor_Left_Set(diffleft+20*Direction);
-    }
-        */
-        
-        // ****************************************************************************************************************************
-        // ****************************************************************************************************************************
-        /*
-        //Test du bloc ultra-son
-        
-        dcenter = Distance_Get(Center);
-        if (dcenter > 14)                       // champs libre devant
-        {
-        Motor_Left_Set(diffleft);
-        Motor_Right_Set(diffright);
-    }
-        
-        dright = Distance_Get(Right);          //Danger à droite 
-        if (dright < 8)
-        {
-        Motor_Left_Set(0);
-        while(dright<10)                      
-        {
-        dright = Distance_Get(right);
-    }
-    }
-        Motor_Left_Set(diffleft);
-        
-        
-        dleft = Distance_Get(Left);            //Danger à gauche
-        if (dleft < 8)
-        {
-        Motor_Right_Set(0);
-        while(dleft<10)                      
-        {
-        dleft = Distance_Get(Left);
-    }
-    }
-        Motor_Right_Set(diffright);
-        
-        
-        dcenter = Distance_Get(Center);
-        if (dcenter < 15)             // danger devant
-        {
-        while (dcenter < 25)
-        {
-        if (dright > dleft)      //Danger devant, champ libre à droite
-        {
-        Motor_Left_Set(diffleft);
-        Motor_Right_Set(0);
-    }
-        
-        if(dright < dleft)      //Danger devant, champ libre à gauche
-        {
-        Motor_Right_Set(diffright);
-        Motor_Left_Set(0);      
-    }
-        dcenter = Distance_Get(Center);
-        dright = Distance_Get(Right);
-        dleft = Distance_Get(Left);
-    }
-    }
-        
-        */
-        // ****************************************************************************************************************************
-        // ****************************************************************************************************************************
-        
-        // Programe final ?
-        //Test de bloc ultra-son + infrarouge
-        
+
         dcenter = Distance_Get(Center);
         if (dcenter >= Distance_US_Front)                     // champs libre devant
         {
@@ -131,8 +40,8 @@ void route(void)
             {
                 Motor_Right_Set(diffright - 35*Direction);
                 Motor_Left_Set(diffleft);
-                
-                
+
+
             }
             if (Direction < 0)
             {
@@ -142,51 +51,51 @@ void route(void)
         }
         else
         {
-           
+
             if (dcenter < Distance_US_Front)        // tant qu'il y a un obstacle devant...
             {
                 delay(Delay_US);
                 dleft = Distance_Get(Left);
                 delay(Delay_US);
                 dright = Distance_Get(Right);
-                
-                
-                if ((dleft < 30) && (dright > dleft))  //...et à gauche...
+
+
+                if ((dleft < 30) && (dright > dleft))  //...et ?gauche...
                 {
-                    Motor_Left_Set(diffleft);          //...on tourne à droite.
+                    Motor_Left_Set(diffleft);          //...on tourne ?droite.
                     Motor_Right_Set(0);
                     while(dcenter<30)
                     {
                         dcenter=Distance_Get(Center);
                     }
                 }
-                
-                if ((dright < 30) && (dright < dleft))  //...et à droite...
+
+                if ((dright < 30) && (dright < dleft))  //...et ?droite...
                 {
-                    Motor_Left_Set(0);                  //...on tourne à gauche.
+                    Motor_Left_Set(0);                  //...on tourne ?gauche.
                     Motor_Right_Set(diffright);
                     while(dcenter<30)
                     {
                         dcenter=Distance_Get(Center);
                     }
                 }
-                
-                
-                if ((dleft > 30) && (dright > 30))      // ...et rien sur les côtés... 
+
+
+                if ((dleft > 30) && (dright > 30))      // ...et rien sur les côtés...
                 {
-                    if (Direction >= 0 )                //...et la balise à droite...
+                    if (Direction >= 0 )                //...et la balise ?droite...
                     {
-                        Motor_Left_Set(diffleft);       // on tourne à droite
+                        Motor_Left_Set(diffleft);       // on tourne ?droite
                         Motor_Right_Set(-50);
                         while(dcenter<30)
                         {
                             dcenter=Distance_Get(Center);
                         }
                     }
-                    
-                    else                           //...et la balise à gauche...
+
+                    else                           //...et la balise ?gauche...
                     {
-                        Motor_Left_Set(-50);       // on tourne à gauche
+                        Motor_Left_Set(-50);       // on tourne ?gauche
                         Motor_Right_Set(diffright);
                         while(dcenter<30)
                         {
@@ -195,53 +104,53 @@ void route(void)
                     }
                 }
             }
-        }        
-                
-                
-        
-        dright = Distance_Get(Right);          //Danger à droite
+        }
+
+
+
+        dright = Distance_Get(Right);          //Danger ?droite
         if (dright < Distance_US_Side)
         {
             Motor_Right_Set(diffright);        //On tourne...
-            Motor_Left_Set(-80); 
-            while(dright<Distance_US_Side)          //...jusqu'à ce que le danger soit écarté            
+            Motor_Left_Set(-80);
+            while(dright<Distance_US_Side)          //...jusqu'?ce que le danger soit écart?
             {
                 dright = Distance_Get(Right);
                 delay(20);
             }
             Motor_Left_Set(diffleft);
         }
-        
-        
-        
 
-        dleft = Distance_Get(Left);            //Danger à gauche
-        dleft = Distance_Get(Left);            //Danger à gauche
+
+
+
+        dleft = Distance_Get(Left);            //Danger ?gauche
+        dleft = Distance_Get(Left);            //Danger ?gauche
         if (dleft < Distance_US_Side)
         {
             Motor_Left_Set(diffleft);          //On tourne...
-            Motor_Right_Set(-80); 
-            while(dleft<Distance_US_Side)           //...jusqu'à ce que le danger soit écarté                
+            Motor_Right_Set(-80);
+            while(dleft<Distance_US_Side)           //...jusqu'?ce que le danger soit écart?
             {
                 dleft = Distance_Get(Left);
                 delay(20);
             }
             Motor_Right_Set(diffright);
         }
-        
-        
-        
-        
+
+
+
+
         // ****************************************************************************************************************************
         // ****************************************************************************************************************************
         /*
         //Test tout
-        
+
         pright = diffright;
         pleft = diffleft;
-        
+
         // *************************************************
-        // Direction par rapport à la balise
+        // Direction par rapport ?la balise
         if (Direction > 0)
         {
         pright = pright - 20*Direction;
@@ -250,19 +159,19 @@ void route(void)
         {
         pleft = pleft + 20*Direction;
     }
-        
+
         // *************************************************
         // direction par rapport au US
         dright = Distance_Get(Right);
         if (dright < 7)
         pleft = 0;
         pright = diffright;
-        
+
         dleft = Distance_Get(Left);
         if (dleft < 7)
         pright = 0;
         pleft = diffleft;
-        
+
         dcenter = Distance_Get(Center);
         if (dcenter < 20)
         {
@@ -273,15 +182,15 @@ void route(void)
         pright = diffright;
         pleft = 0;
     }
-        
+
         if (dright < 10 && dright>6 && dleft < 10 && dleft > 6 && dcenter> 40)
         {
         pleft = diffleft;
         pright = diffright;
     }
-        
+
         // *************************************************
-        
+
         Motor_Left_Set(pleft);
         Motor_Right_Set(pright);
         */
@@ -304,7 +213,7 @@ void final(void)
     BALLON_STOP;
     while(1)
     {
-        
+
     }
 }
 
@@ -324,7 +233,7 @@ void BUMP_Left(void)
 
 void BUMP_Center(void)
 {
-    
+
 }
 
 void BUMP_Right(void)
